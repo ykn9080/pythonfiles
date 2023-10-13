e2a1af79f551b25b23b8fe3167a7592d9232f702
young <ykn9080@empal.com>
2023-10-13 10:20:35 +0900

    sparkrddtoDF

--- pyspark/sparkRdd1.py
+++ pyspark/sparkRdd1.py
@@ -3,13 +3,20 @@
 from pyspark import SparkContext
 
 sc = SparkContext("local", "first app")
+# from pyspark.sql import SparkSession
+# spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+# sc = spark.sparkContext
 
-rdd = sc.parallelize([1, 2, 3, 4, 5])
+print("\n------------ Basic ------------------------------------------\n")
+animals = ("dog", "cat", "frog", "horse")
+rdd = sc.parallelize(animals)
 print("getNumPartitions: ", rdd.getNumPartitions())
 print("first: ", rdd.first())
 print("take(1): ", rdd.take(1))
 print("count: ", rdd.count())
-print("------------ Word Count ------------------------------------------")
+print("filter", rdd.filter(lambda x: "o" in x).collect())
+
+print("\n------------ Word Count ------------------------------------------\n")
 rdd1 = sc.textFile("/data/wordSample.txt")
 print("rdd: ", rdd1.collect())
 rddcsv = rdd1.flatMap(lambda line: line.split(" "))
@@ -18,3 +25,6 @@ rddgrp = rddcsv.map(lambda word: (word, 1))
 print("groupby: ", rddgrp.collect())
 rddcount = rddgrp.reduceByKey(lambda a, b: a + b)
 print("reduceByKey: ", rddcount.collect())
+
+
+spark.stop()
---
+++ pyspark/sparkRddToDF.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+from pyspark.sql.types import StructType, StructField, StringType
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.master("local[1]").appName(
+    'SparkByExamples.com').getOrCreate()
+
+dept = [("Finance", 10), ("Marketing", 20), ("Sales", 30), ("IT", 40)]
+rdd = spark.sparkContext.parallelize(dept)
+
+df = rdd.toDF()
+df.printSchema()
+df.show(truncate=False)
+
+deptColumns = ["dept_name", "dept_id"]
+df2 = rdd.toDF(deptColumns)
+df2.printSchema()
+df2.show(truncate=False)
+
+deptDF = spark.createDataFrame(rdd, schema=deptColumns)
+deptDF.printSchema()
+deptDF.show(truncate=False)
+
+deptSchema = StructType([
+    StructField('dept_name', StringType(), True),
+    StructField('dept_id', StringType(), True)
+])
+
+deptDF1 = spark.createDataFrame(rdd, schema=deptSchema)
+deptDF1.printSchema()
+deptDF1.show(truncate=False)
+
+spark.stop()
